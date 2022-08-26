762cb5495889343fcbe6660ee8bc1acd6b76fcaf
KautTanya <kaut.tanya.s@gmail.com>
2022-08-27 00:45:55 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryImg = document.querySelector('.gallery');
+const cardImg = createGallery(galleryItems);
 
+galleryImg.insertAdjacentHTML('beforeend', cardImg);
+
+galleryImg.addEventListener('click', onCardImgClick);
+
+
+function createGallery(galleryItems){
+    return galleryItems.map(({preview, original, description}) =>{
+        return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        `
+    })
+    .join('');
+}
+function onCardImgClick(event){
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+      }
+      
+      let gallery = new SimpleLightbox('.gallery a');
+      gallery.on("show.simplelightbox", function () {
+       
+      });
+  
+};
 console.log(galleryItems);
